f4248cbfbc91caa282b923d5ef0fa0df859f38a4
Nuel King <166351729+engrigweemmanuel112@users.noreply.github.com>
2025-02-14 14:12:01 +0000

    initial commit

---
+++ onclick-project.js
@@ -0,0 +1,6 @@
+function myOnclickFunction(){
+ 
+  var headingElement = document.getElementById("learn"); // here: we target the id of the HTML heading element
+
+  headingElement.textContent = "LEARNING ONCLICK FUNCTION"; // here: we assign the new text to the Html heading element- ie- assign what you what to see when the text changes whrn clicked.
+}
